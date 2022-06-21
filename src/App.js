a132cb9a31c2e56be4235bd96df37f6a73a9d4d2
unknown <sthaayush08@gmail.com>
2022-06-21 18:31:50 +0545

    react tutorial files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import ExpenseItem from "./component/ExpenseItem";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Header</h1>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>Date</div>
+      <div>
+        <h1>title</h1>
+        <div>Amount</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
